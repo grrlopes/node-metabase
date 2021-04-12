38d5d4c6834801e36f3c391c4731d31273d595e6
Gabriel Lopes <gabrielrrlopes@gmail.com>
2021-04-12 04:11:16 -0300

    Fixed scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-source .env
+if [ -f .env ]; then
+  source .env
+fi
 
 apt update
 apt install ansible -y
 echo "##############"
 echo "Next step will"
-echo  "be play BooK"
+echo "be play BooK"
 echo "##############"
 sleep 8
 ansible-playbook ./scripts/book/metabase.yml
@@ -16,7 +18,7 @@ echo "###################"
 echo "Making sure the api has"
 echo " been reached out ADM_ID"
 echo "###################"
-env |grep -i ^META
+env | grep -i ^META
 echo "###################"
 
 echo "####"
@@ -28,4 +30,3 @@ npm audit fix
 npm run build
 npm run migration
 npm run test
-
